3e88005e811b13efaa421cb2d915cc3a0d5cc1ce
YuxiBro <yuxi.wangcd@gmail.com>
2016-03-03 20:24:37 -0700

    move files around

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+var path = require("path");
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+    res.sendFile(path.resolve(__dirname+"/../views/medy.html"));
+    //console.log(__dirname+"../views/medy.html");
 });
 
 module.exports = router;
